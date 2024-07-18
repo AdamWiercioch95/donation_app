1a8bd342b4326249e7d7201d28cd560100ea3c93
Adam Wiercioch <adamwiercioch95@gmail.com>
2024-07-18 11:50:26 +0200

    dynamic view for quantity of donated bags and supported institutions, refactor index.html

--- project_app/views.py
+++ project_app/views.py
@@ -1,13 +1,22 @@
-from django.shortcuts import render
+from django.db.models import Sum
 from django.views.generic import TemplateView
+
+from project_app.models import Donation, Institution
 
 
 class LandingPageView(TemplateView):
     template_name = 'index.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['total_bags'] = Donation.objects.all().aggregate(Sum('quantity'))['quantity__sum'] or 0
+        context['institutions'] = Institution.objects.count()
+        return context
+
 
 class AddDonationView(TemplateView):
     template_name = 'form.html'
+
 
 class ConfirmDonationView(TemplateView):
     template_name = 'form-confirmation.html'
