c57e0753db8f1c2c1fc023afd77f8c5ad609e933
Adam Wiercioch <adamwiercioch95@gmail.com>
2024-07-18 11:25:38 +0200

    category, institution, donation models with migration

--- project_app/models.py
+++ project_app/models.py
@@ -1,3 +1,43 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+TYPES = (
+    (1, 'fundacja'),
+    (2, 'organizacja pozarządowa'),
+    (3, 'zbiórka lokalna'),
+)
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    type = models.IntegerField(choices=TYPES, default=1)
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.name
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=9)
+    city = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=6)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.TextField(blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f'Quantity: {self.quantity}, Institution: {self.institution}'
