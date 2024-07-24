3feb3f4f7de5d81c42b418da63a41446e162902b
Adam Wiercioch <adamwiercioch95@gmail.com>
2024-07-24 13:27:42 +0200

    save donation in database after form submit, form confirmation display

--- project_app/views.py
+++ project_app/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.db.models import Sum
+from django.http import HttpResponseBadRequest
 from django.shortcuts import redirect, render
 from django.views import View
 from django.views.generic import TemplateView
@@ -42,9 +43,47 @@ class AddDonationView(LoginRequiredMixin, View):
 
         return render(request, 'form.html', context)
 
+    def post(self, request):
+        quantity = request.POST.get('bags')
+        categories = request.POST.getlist('categories')
+        institution_id = request.POST.get('organization')
+        address = request.POST.get('address')
+        phone_number = request.POST.get('phone')
+        city = request.POST.get('city')
+        zip_code = request.POST.get('postcode')
+        pick_up_date = request.POST.get('data')
+        pick_up_time = request.POST.get('time')
+        pick_up_comment = request.POST.get('more_info')
 
-class ConfirmDonationView(TemplateView):
-    template_name = 'form-confirmation.html'
+        if not all([quantity, institution_id, address, phone_number, city, zip_code, pick_up_date, pick_up_time]):
+            return HttpResponseBadRequest("Missing required fields.")
+
+        try:
+            institution = Institution.objects.get(id=institution_id)
+        except Institution.DoesNotExist:
+            return HttpResponseBadRequest("Invalid organization.")
+
+        donation = Donation(
+            quantity=quantity,
+            address=address,
+            phone_number=phone_number,
+            city=city,
+            zip_code=zip_code,
+            pick_up_date=pick_up_date,
+            pick_up_time=pick_up_time,
+            pick_up_comment=pick_up_comment,
+            user=request.user,
+            institution=institution
+        )
+        donation.save()
+        donation.categories.set(categories)
+
+        return redirect('confirm')
+
+
+class ConfirmDonationView(LoginRequiredMixin, View):
+    def get(self, request):
+        return render(request, 'form-confirmation.html')
 
 
 class LoginView(TemplateView):
