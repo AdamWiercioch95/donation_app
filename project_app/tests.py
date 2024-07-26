957e54ced56e462e40ffd784966cda93683e7321
Adam Wiercioch <adamwiercioch95@gmail.com>
2024-07-26 12:28:44 +0200

    login and logout tests

--- project_app/tests.py
+++ project_app/tests.py
@@ -1,4 +1,5 @@
 import pytest
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.urls import reverse
 
@@ -63,3 +64,61 @@ def test_register_view_existing_user(client, user):
     assert response.status_code == 200
     assert 'Użytkownik o podanym adresie email już istnieje.' in response.content.decode()
     assert User.objects.filter(email='testuser@op.pl').count() == 1
+
+
+@pytest.mark.django_db
+def test_login_view_success(client, user):
+    url = reverse('login')
+    data = {
+        'email': 'testuser@op.pl',
+        'password': 'password'
+    }
+    response = client.post(url, data)
+
+    assert response.status_code == 302
+    assert response.url == reverse('landing_page')
+
+
+@pytest.mark.django_db
+def test_login_view_invalid_password(client, user):
+    url = reverse('login')
+    data = {
+        'email': 'testuser@op.pl',
+        'password': 'wrong_password'
+    }
+    response = client.post(url, data)
+
+    assert response.status_code == 200
+    assert 'Nieprawidłowe hasło.' in response.content.decode()
+    assert not response.wsgi_request.user.is_authenticated
+
+
+@pytest.mark.django_db
+def test_login_view_nonexistent_user(client):
+    url = reverse('login')
+    data = {
+        'email': 'nonexistent@user.com',
+        'password': 'password'
+    }
+    response = client.post(url, data)
+
+    assert response.status_code == 302
+    assert response.url == reverse('register')
+    assert not User.objects.filter(email='nonexistent@user.com').exists()
+
+
+@pytest.mark.django_db
+def test_logout_view(client, user):
+    client.login(username='testuser@op.pl', password='password')
+
+    user = get_user_model().objects.get(username='testuser@op.pl')
+    assert user.is_authenticated
+
+    url = reverse('logout')
+    response = client.get(url)
+
+    assert response.status_code == 302
+    assert response.url == reverse('landing_page')
+
+    response = client.get(reverse('landing_page'))
+    assert not response.wsgi_request.user.is_authenticated
