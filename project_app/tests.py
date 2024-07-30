f64d95d108739d6d8cc8f608909332e321a2de16
Adam Wiercioch <adamwiercioch95@gmail.com>
2024-07-30 08:42:35 +0200

    add donation and confirmation tests

--- project_app/tests.py
+++ project_app/tests.py
@@ -3,6 +3,8 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+from project_app.models import Donation
+
 
 @pytest.mark.django_db
 def test_landing_page_view(client, donation):
@@ -122,3 +124,115 @@ def test_logout_view(client, user):
 
     response = client.get(reverse('landing_page'))
     assert not response.wsgi_request.user.is_authenticated
+
+
+@pytest.mark.django_db
+def test_add_donation_view_get(client, user):
+    client.force_login(user)
+    url = reverse('add_donation')
+    response = client.get(url)
+
+    assert response.status_code == 200
+    assert 'categories' in response.context
+    assert 'organizations' in response.context
+    for organization in response.context['organizations']:
+        assert hasattr(organization, 'category_ids_json')
+
+
+@pytest.mark.django_db
+def test_add_donation_view_post_success(client, user, category, institution):
+    client.force_login(user)
+    url = reverse('add_donation')
+    data = {
+        'bags': '3',
+        'categories': [category.id],
+        'organization': institution.id,
+        'address': 'test address',
+        'phone': '111222333',
+        'city': 'test city',
+        'postcode': '12-345',
+        'data': '2024-10-01',
+        'time': '10:00',
+        'more_info': 'Careful',
+    }
+    response = client.post(url, data)
+
+    assert response.status_code == 302
+    assert response.url == reverse('confirm')
+    assert Donation.objects.count() == 1
+    donation = Donation.objects.first()
+    assert donation.quantity == 3
+    assert donation.address == 'test address'
+    assert donation.phone_number == '111222333'
+    assert donation.city == 'test city'
+    assert donation.zip_code == '12-345'
+    assert donation.pick_up_date.strftime('%Y-%m-%d') == '2024-10-01'
+    assert donation.pick_up_time.strftime('%H:%M') == '10:00'
+    assert donation.pick_up_comment == 'Careful'
+    assert donation.institution == institution
+    assert list(donation.categories.all()) == [category]
+
+
+@pytest.mark.django_db
+def test_add_donation_view_post_missing_fields(client, user):
+    client.force_login(user)
+    url = reverse('add_donation')
+    data = {
+        'bags': '',
+        'categories': [],
+        'organization': '',
+        'address': '',
+        'phone': '',
+        'city': '',
+        'postcode': '',
+        'data': '',
+        'time': '',
+        'more_info': '',
+    }
+    response = client.post(url, data)
+
+    assert response.status_code == 400
+    assert 'Missing required fields.' in response.content.decode()
+
+
+@pytest.mark.django_db
+def test_add_donation_view_post_invalid_organization(client, user):
+    client.force_login(user)
+    url = reverse('add_donation')
+    data = {
+        'bags': '3',
+        'categories': [1],
+        'organization': 999,  # Invalid organization ID
+        'address': '123 Main St',
+        'phone': '555-555-5555',
+        'city': 'Sample City',
+        'postcode': '12345',
+        'data': '2024-10-01',
+        'time': '10:00',
+        'more_info': 'Leave at the door',
+    }
+    response = client.post(url, data)
+
+    assert response.status_code == 400
+    assert 'Invalid organization.' in response.content.decode()
+
+
+@pytest.mark.django_db
+def test_confirm_donation_view_get_authenticated(client, user):
+    client.force_login(user)
+    url = reverse('confirm')
+    response = client.get(url)
+
+    assert response.status_code == 200
+    assert 'form-confirmation.html' in [t.name for t in response.templates]
+
+
+@pytest.mark.django_db
+def test_confirm_donation_view_get_unauthenticated(client):
+    url = reverse('confirm')
+    response = client.get(url)
+
+    assert response.status_code == 302
+    assert response.url.startswith('/accounts/login/')
+    assert 'next=' in response.url
+    assert response.url == f"/accounts/login/?next={url}"
